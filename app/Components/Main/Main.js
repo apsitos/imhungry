47900cee2fe738ee597138e90c0ad9cd8bd8b725
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-02-22 13:58:20 -0700

    Begin adding functions to call API

--- app/Components/Main/Main.js
+++ app/Components/Main/Main.js
@@ -1,23 +1,48 @@
 import React from 'react';
 import Header from '../Header/Header';
+import Button from '../Button/Button';
 require('./main-styles');
 // import getPlaces from '../../../src/controller'
 
 export default class Main extends React.Component {
-  componentDidMount() {
+  constructor() {
+    super();
+    this.state= {
+      address:''
+    }
+  }
+
+  geocodeAddress() {
+    let geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ address: this.state.address }, function(results, status){
+      if(status == 'OK') {
+        console.log(results);
+      }
+    })
+  }
+
+
+  getBars() {
     fetch(`/api/places`)
-      .then((response) => {
-        console.log(response)
+    .then((response) => {
+      console.log(response)
       return response.json()
     }).then((data) => {
       console.log(data)
     })
   }
 
+
   render() {
     return(
       <div>
         <Header />
+        <input type='text'
+          value={this.state.address}
+          onChange={ (e)=>this.setState({ address: e.target.value }) }
+          placeholder='Enter your address'
+        />
+        <Button id='findBars' handleClick={this.getCoordinates.bind(this)} name="Find a Bar!" />
       </div>
     )
   }
