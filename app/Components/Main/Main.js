58ffa0353a4d5223ec601d7e7a73f976047da3ca
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-02-23 13:47:16 -0700

    Input of address saves to state, API call saves to state

--- app/Components/Main/Main.js
+++ app/Components/Main/Main.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Header from '../Header/Header';
 import Button from '../Button/Button';
-const API_KEY = `AIzaSyC0tw-FgBeIrwwIYl6pf5M_e7IqC92cfx4`
-const COORDS_URL = `https://maps.googleapis.com/maps/api/geocode/json?address=`
 require('./main-styles');
 // import getPlaces from '../../../src/controller'
 
@@ -12,7 +10,8 @@ export default class Main extends React.Component {
     this.state= {
       address:'',
       lat: '',
-      long: ''
+      long: '',
+      allBars: []
     }
   }
 
@@ -20,23 +19,12 @@ export default class Main extends React.Component {
     this.setState({ address: e.target.value })
   }
 
-  getCoords(){
-    fetch(`${COORDS_URL}${this.state.address}&key=${API_KEY}`)
-    .then((res) => {
-      return res.json()
-    }).then((res) => {
-      this.setState({
-        lat:res.results[0].geometry.location.lat,
-        lon:res.results[0].geometry.location.lng
-      })
-    })
-  }
-
   getBars(location) {
     fetch(`/api/places`)
     .then((response) => {
       return response.json()
     }).then((data) => {
+      this.setState({ allBars: data });
     })
   }
 
@@ -45,10 +33,12 @@ export default class Main extends React.Component {
       <div>
         <Header />
         <input type='text'
-          onChange={ (e) => }
+          value = {this.state.address}
+          onChange={ (e) => this.setLocation(e) }
           placeholder='Enter your address'
         />
-        <Button id='findBars' handleClick={this.getCoords.bind(this)} name="Find a Bar!" />
+        <Button id='findBars' handleClick={this.getBars.bind(this)} name="Find a Bar!" />
+        <
       </div>
     )
   }
