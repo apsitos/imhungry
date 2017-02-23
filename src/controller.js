3185bf571d4676fbbb7151c4c1240ff1a46ba6c0
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-02-23 13:49:25 -0700

    Attempt native geolocation function

--- src/controller.js
+++ src/controller.js
@@ -1,14 +1,20 @@
 const request = require('request');
 const API_KEY = `AIzaSyC0tw-FgBeIrwwIYl6pf5M_e7IqC92cfx4`
-const BASE_URL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=39.7257155,-104.9713034&radius=1600&keyword=restaurant&pagetoken&key=${API_KEY}`
 
 function getPlaces(req, res, next) {
-  request(BASE_URL, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      res.send(body)
-    }
+  navigator.geolocation.getCurrentPosition((position) => {
+    const lat = position.coords.latitude
+    const long = position.coords.longitude
+    const BASE_URL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${long}&radius=1600&keyword=restaurant&pagetoken&key=${API_KEY}`
+    request(BASE_URL, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        res.send(body)
+      }
+    })
   })
 }
+
+
 
 module.exports = {
   getPlaces: getPlaces
