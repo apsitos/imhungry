8d51ca3239269280f47e710d9e84771e93208e3c
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-02-24 13:11:43 -0700

    Add geolocation API call

--- app/Helpers/Coordinates.js
+++ app/Helpers/Coordinates.js
@@ -1,27 +1,18 @@
-import React from 'react';
 
-export default class Coordinates extends React.Component{
-  constructor(props){
-    super(props);
-    this.state= {
-      lat: '',
-      long: ''
-    }
-  }
+const getCoords = () => {
 
-  getCoords(props) {
-    const API_KEY = `AIzaSyC0tw-FgBeIrwwIYl6pf5M_e7IqC92cfx4`;
-    const COORDS_URL = `https://maps.googleapis.com/maps/api/geocode/json?address=`;
-    fetch(`${COORDS_URL}${this.props.address}&key=${API_KEY}`)
-    .then((res) => {
-      return res.json()
-    }).then((res) => {
-      this.setState({
-        lat:res.results[0].geometry.location.lat,
-        lon:res.results[0].geometry.location.lng
-      })
+  const API_KEY = `AIzaSyC0tw-FgBeIrwwIYl6pf5M_e7IqC92cfx4`;
+  const COORDS_URL = `https://maps.googleapis.com/maps/api/geocode/json?address=`;
+
+  fetch(`${COORDS_URL}${this.props.address}&key=${API_KEY}`)
+  .then((res) => {
+    return res.json()
+  }).then((res) => {
+    this.setState({
+      lat:res.results[0].geometry.location.lat,
+      lon:res.results[0].geometry.location.lng
     })
-  }
-  
-  getCoords();
+  })
 }
+
+export default getCoords;
