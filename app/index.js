94f4be66a1b94ad8641e62d7620a94cb2014cf5c
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-02-24 13:12:03 -0700

    removed file

--- app/index.js
+++ app/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { Router, hashHistory, Route, IndexRoute } from 'react-router';
+import rootReducer from './reducers'
 import Main from './Components/Main/Main';
 import Location from './Components/Location/Location'
 
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const store = createStore(rootReducer, devTools);
 
 render(
-  <Router>
-    <Route path="/" component={Main}>
-      <Route path='location' component={Location}/>
-    </Route>
-  </Router>
+  <Provider >
+    <Router browserhistory={hashHistory}>
+      <Route path="/" component={Main}>
+        <Route path='location' component={Location}/>
+      </Route>
+    </Router>
+  </Provider>
   , document.querySelector('.application'))
