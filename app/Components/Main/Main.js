98ee777450c51dba2c767272d2e847872dbe72b7
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-02-25 23:08:53 -0700

    Pass dynamic lat and long to API backend, fix merge conflicts

--- app/Components/Main/Main.js
+++ app/Components/Main/Main.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Link } from 'react-router';
 import Header from '../Header/Header';
 import Button from '../Button/Button';
-import Location from '../Location/Location';
-import getCoords from '../Helpers/Coordinates';
+// import Location from '../Location/Location';
+// import getCoords from '../../Helpers/Coordinates'
 require('./main-styles');
 
 export default class Main extends React.Component {
@@ -21,8 +21,37 @@ export default class Main extends React.Component {
     this.setState({ address: e.target.value })
   }
 
+  // formatAddress() {
+  //   let formattedAddress = this.state.address.trim().split(' ')
+  //   .map(i => i.concat('+')).join()
+  //   this.setState({ address: formattedAddress})
+  // }
+  //
+  // getCoords (address) {
+  //   const API_KEY = `AIzaSyC0tw-FgBeIrwwIYl6pf5M_e7IqC92cfx4`;
+  //   const COORDS_URL = `https://maps.googleapis.com/maps/api/geocode/json?address=`;
+  //
+  //   fetch(`${COORDS_URL}${address}&key=${API_KEY}`)
+  //   .then((res) => {
+  //     console.log(res)
+  //     return res.json()
+  //   }).then((res) => {
+  //     this.setState({
+  //       lat:res.results[0].geometry.location.lat,
+  //       lon:res.results[0].geometry.location.lng
+  //     }).catch(() => {
+  //       console.log('location, please');
+  //     })
+  //   })
+  // }
+
   getBars(location) {
-    fetch(`/api/places?lat=${this.state.lat}&long=${this.state.long}`)
+    // this.formatAddress()
+    // .then(() => this.getCoords(this.state.address))
+    // .then((lat, long) => {
+    //   console.log(lat, long)
+    //   this.
+      fetch(`/api/places?lat=${this.state.lat}&long=${this.state.long}`)
     .then((response) => {
       return response.json()
     }).then((data) => {
@@ -42,9 +71,9 @@ export default class Main extends React.Component {
           onChange={ (e) => this.setLocation(e) }
           placeholder='Enter your address'
         />
-          <Button id='findBars' handleClick={this.getBars.bind(this)} name="Find a Bar!" />
+          <Button id='findBars' handleClick={this.getBars.bind(this)} name="Find a Bar!"/>
         <div>
-          <Location />
+          words
         </div>
       </div>
     )
--- app/Helpers/Coordinates.js
+++ app/Helpers/Coordinates.js
@@ -1,16 +1,19 @@
 
-const getCoords = () => {
+const getCoords = (address) => {
 
   const API_KEY = `AIzaSyC0tw-FgBeIrwwIYl6pf5M_e7IqC92cfx4`;
   const COORDS_URL = `https://maps.googleapis.com/maps/api/geocode/json?address=`;
 
-  fetch(`${COORDS_URL}${this.props.address}&key=${API_KEY}`)
+  fetch(`${COORDS_URL}${address}&key=${API_KEY}`)
   .then((res) => {
+    console.log(res)
     return res.json()
   }).then((res) => {
     this.setState({
       lat:res.results[0].geometry.location.lat,
       lon:res.results[0].geometry.location.lng
+    }).catch(() => {
+      console.log('location, please');
     })
   })
 }
