681c41875bbc766b0c319a2a5939e40898d9d64e
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-02-26 14:18:52 -0700

    Render address and name

--- app/Components/Location/Location.js
+++ app/Components/Location/Location.js
@@ -9,17 +9,22 @@ export default class Location extends React.Component {
     }
   }
 
+  // openNow() {
+  //   if(this.props.bars.opening_hours.open_now===true) 'Yes!'
+  // }
 
-allBars() {
-  return this.props.bars.map((barObj) => {
-    console.log(barObj)
-    return(
-      <div key={barObj.id}>
-        <h2>{barObj.name}</h2>
-      </div>
-    )
-  })
-}
+  allBars() {
+    return this.props.bars.map((barObj) => {
+      console.log(barObj)
+      return(
+        <div key={barObj.id}>
+          <h3 className='name'>{barObj.name}</h3>
+          {/* <h4>Open now: {this.openNow()} </h4> */}
+          <h4 className='address'>{barObj.vicinity}</h4>
+        </div>
+      )
+    })
+  }
 
   render() {
     return(
