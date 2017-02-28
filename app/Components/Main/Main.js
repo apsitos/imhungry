d1925e05863267ae02fc690dc7b80b052b0804b6
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-02-28 15:54:01 -0700

    Update wireframe, make changes to API functionality

--- app/Components/Main/Main.js
+++ app/Components/Main/Main.js
@@ -11,7 +11,7 @@ export default class Main extends React.Component {
     super(props);
     this.state= {
       address:'',
-      formattedAddress: '',
+      // formattedAddress: '',
       lat: '',
       long: '',
       barArray: []
@@ -20,49 +20,58 @@ export default class Main extends React.Component {
 
   setLocation(e) {
     this.setState({ address: e.target.value })
+    // this.setState({ address: '90 Corona St' })
   }
 
   formatAddress() {
-    let formattedAddress = this.state.address.trim().split(' ')
+    debugger
+    return this.state.address.trim().split(' ')
     .map(i => i.concat('+')).join()
-    this.setState({ formattedAddress: formattedAddress})
   }
 
   getCoords() {
     const API_KEY = `AIzaSyC0tw-FgBeIrwwIYl6pf5M_e7IqC92cfx4`;
     const COORDS_URL = `https://maps.googleapis.com/maps/api/geocode/json?address=`;
-    console.log(this.state.address)
-    fetch(`${COORDS_URL}${this.state.address}&key=${API_KEY}`)
-    .then(res => {
-      console.log(res)
-      return res.json()
-    }).then(res => {
-      this.setState({
-        lat: res.results[0].geometry.location.lat,
-        long: res.results[0].geometry.location.lng
+
+    return new Promise ((resolve, reject) => {
+      const formattedAddress = this.formatAddress();
+      fetch(`${COORDS_URL}${formattedAddress}&key=${API_KEY}`)
+      .then(res => {
+        return res.json();
       })
-    })
-    .catch(() => {
-      console.log('getCoords: nope');
+      .then(data => {
+        if (data.results) {
+          this.setState({
+            lat: data.results[0].geometry.location.lat,
+            long: data.results[0].geometry.location.lng,
+          });
+          return resolve();
+        } else {
+          reject();
+        }
+      })
+      .catch((err) => {
+        throw new Error(err);
+      })
     })
   }
 
   getBars(location) {
     fetch(`/api/places?lat=${this.state.lat}&long=${this.state.long}`)
     .then((response) => {
-      return response.json()
-    }).then((data) => {
+      const data = response.json();
       this.setState({ barArray: data.results });
     })
-    .catch(() => {
-      console.log('getBars: nope')
+    .catch((err) => {
+      throw new Error ('getBars' ,err);
     })
   }
 
   showBars() {
-    this.formatAddress()
-    .then(this.getCoords())
-    .then(this.getBars(location))
+    // this.formatAddress()
+    // .then(this.getCoords())
+    // .then(this.getBars(location))
+    this.getCoords().then(this.getBars)
   }
 
 
