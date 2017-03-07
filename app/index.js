62887b85664e541e9e03aed60f36eff40cbddabb
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-03-07 15:02:26 -0700

    Wire up Router

--- app/index.js
+++ app/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, hashHistory, Route, IndexRoute } from 'react-router';
 import Main from './Components/Main/Main';
-import Location from './Components/Location/Location'
+import Location from './Components/Location/Location';
+import Landing from './Components/Landing/Landing';
 
 render(
     <Router history={hashHistory}>
       <Route path="/" component={Main}>
+        <IndexRoute component={Landing} />
         <Route path='/location' component={Location}/>
       </Route>
     </Router>
