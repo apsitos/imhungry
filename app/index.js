516cb64c7ed6cc733f03c893a50c6110cf28be7c
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-03-30 10:12:36 -0600

    Rename remote

--- app/index.js
+++ app/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, hashHistory, Route, IndexRoute } from 'react-router';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers'
 import Main from './Components/Main/Main';
 import Location from './Components/Location/Location';
 import Landing from './Components/Landing/Landing';
 
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const store = createStore(rootReducer, devTools);
+
 render(
-  <Provider>
+  <Provider store={ store }>
     <Router history={hashHistory}>
       <Route path="/" component={Main}>
         <IndexRoute component={Landing} />
--- app/store.js
+++
@@ -1,4 +0,0 @@
-import { createStore, compose } from 'redux';
-import { syncHistoryWithStore } from 'react-router-redux';
-import { browserHistore } from 'react-router';
-import rootReducer from './reducers/index';
