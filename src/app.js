57ac0a51f500406290b3cb28eb0fff2bd531a562
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-04-06 13:38:35 -0600

    Update server file

--- src/app.js
+++ src/app.js
@@ -10,22 +10,22 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// if (process.env.NODE_ENV !== 'production') {
-//   const webpack = require('webpack');
-//   const webpackDevMiddleware = require('webpack-dev-middleware');
-//   const webpackHotMiddleware = require('webpack-hot-middleware');
-//   const config = require('../webpack.config.js');
-//   const compiler = webpack(config);
-//
-//   app.use(webpackHotMiddleware(compiler));
-//   app.use(webpackDevMiddleware(compiler, {
-//     noInfo: true,
-//     publicPath: config.output.publicPath
-//   }));
-// }
+if (process.env.NODE_ENV !== 'production') {
+  const webpack = require('webpack');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const config = require('../webpack.config.js');
+  const compiler = webpack(config);
+
+  app.use(webpackHotMiddleware(compiler));
+  app.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+  }));
+}
 
 app.use('/assets', express.static(path.join(__dirname, '../app/assets')));
-app.use(express.static('build')) 
+app.use(express.static('build'))
 
 app.get('/', function (req, res) { res.sendFile(path.join(__dirname, '/../build/index.html')) });
 
